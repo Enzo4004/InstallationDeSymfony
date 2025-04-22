95b4eb0397d14b6a39bcacdabebbf556e9d6da6d
Enzo Ramon <enzo.ramon@lapiscine.pro>
2025-04-22 14:14:54 +0200

    about page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,19 +1,40 @@
-<?php 
+<?php
+
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 
-//je crée la class HomeController 
+// je créé une classe HomeController
+class PageController {
 
-class HomeController{
 
-    //je crée la fonction (methode) nomé home  qui affiche un var_dump 
-    //je crée une route qui definit le  chemin (url) à "\"
-     //donc quand l'utilisateur demande l'url"\" ma fonction s'execute  
+	// je créé une fonction (méthode), nomée home, qui affiche un var_dump
+	// je créé une annotation Route, qui définit le chemin (url) à "/" et un nom unique pour la page créée
+	// donc, quand un utilisateur demande l'url "/", ma fonction sera appelée
+	#[Route("/", name: "home")]
+	public function home() {
+		var_dump('Page accueil'); die;
+	}
 
-     #[Route('/', name:"home")]
-     public function home() {
-         var_dump('Page accueil de mon site internet'); die;
-     }
- 
+	#[Route("/contact", name: "contact")]
+	public function contact() {
+		var_dump("Page de contact"); die;
+	}
+
+
+	#[Route("/a-propos", name: "about")]
+	public function about() {
+		var_dump("Page à propos"); die;
+	}
+
 }
+
+
+// récupère l'url demandée
+// analayse l'url et récupère la fin ("/", ou "contact" etc)
+// il regarde si dans tous les controleurs créés, il y a une fonction qui a une annotation Route
+// avec la même url que celle demandée par l'utilisateur
+// si oui, il execute la fonction en dessous de l'annotation :
+// il va instancier automatiquement la classe : 
+// $homeController = new HomeController
+// $homeController->home();
