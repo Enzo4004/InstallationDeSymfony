e920554a57a7f33dcd83720d2dfb3b3eaa1b2e28
Enzo Ramon <enzo.ramon@lapiscine.pro>
2025-04-22 14:47:53 +0200

    response http controller

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -6,19 +6,53 @@ namespace App\Controller;
 
 // Remplace le require
 // On indique ici le namespace de la classe qu'on veut utiliser et Symfony + composer font le require automatiquement
+
+use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 // Création d'une classe "PageController"
 class PageController {
+    
+    #[Route("/", name: "home")]
+	public function home() {
+		// j'utilise la classe Response de Symfony
+		// pour renvoyer une réponse HTTP, incluant HTML et un status HTTP 200
+		return new Response ("<p> coucou ca va bien ? </p>", 200);
+	}
 
-    // Quand on demande l'url "/" ma fonction est appelée
-    // Création d'une annotation Route (page)
-    #[Route('/', name:'home')]
 
-    // Création d'une fonction "home"
-    public function home(){
+}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      /* Création d'une fonction "home"
+      public function home(){
         var_dump('Page accueil'); die;
-    }
+    
 
     #[Route('/contact', name:'contact')]
 
@@ -34,3 +68,4 @@ class PageController {
         var_dump('Page a-propos'); die;
     }
 }
+    */
